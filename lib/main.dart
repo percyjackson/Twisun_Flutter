8dc300200d2b72cadf3ee370b18e7f2cd837cd60
Personal-BitBucket <yeison-arley25@hotmail.com>
2020-12-01 11:34:08 -0500

    Trying handling errors with Crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:twisun/routs.dart';
 import 'package:twisun/screens/home/home_screen.dart';
 import 'package:twisun/screens/splash/splash_screen.dart';
 import 'package:twisun/theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  //await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
   runApp(MyApp());
 }
 
@@ -14,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Twi-Sun',
       theme: theme(),
       //home: SplashScreen(),
       // We use routeName so that we dont need to remember the name
